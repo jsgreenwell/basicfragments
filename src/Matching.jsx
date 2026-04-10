05ee46592ef5838a391b783fbbfb19081487362f
JGreenwell <jsgreenwell83@gmail.com>
2026-04-10 18:11:40 -0500

    Switch to using components for Board - first setup

--- src/Matching.jsx
+++ src/Matching.jsx
@@ -1,70 +1,72 @@
 import './game.css';
-import {useState} from "react";
 
 const BACK = "🎴"
+const cards = [
+    {id: 1, flipped: true, value: "😭", match: false},
+    {id: 2, flipped: true, value: "🦁", match: false},
+    {id: 3, flipped: false, value: "🦆", match: false},
+    {id: 4, flipped: false, value: "😭", match: false},
+    {id: 5, flipped: true, value: "🦆", match: false},
+    {id: 6, flipped: true, value: "🦁", match: false}
+]
+
+
+function updateCard({id}) {
+    return id
+}
+
+function Board() {
+    // This will just render the board
+    // Yes, this is the hard way, but it helps to figure out the components
+
+    let board = []
+    // to hold all start/end ids (start will always have remainder of 1 (1 past 3) and 0 for end
+    let indices = []
+
+    for (let i = 1; i <= cards.length; i++) {
+        if (i%3 === 0) {
+            // Slice ends with length (index to length) so +1 - hate JS slice
+            indices.push(i)
+        } else if (i%3 === 1) {
+            // Slice starts with index so minus 1
+            indices.push(i-1)
+        }
+    }
+
+    // Now its dynamic
+    for (let i = 0; i < indices.length; i+=2) {
+        board.push(<BoardRow start={indices[i]} end={indices[i+1]} />)
+    }
+
+    return board
+}
+
+function BoardRow({start, end}) {
+    return (
+        <tr>
+            {
+                cards.slice(start, end).map(card => {
+                    return (
+                        <td>
+                        <button className="btn-game" onClick={() => updateCard(card.id)}>
+                            { card.flipped ? card.value : BACK }
+                        </button>
+                        </td>
+                    )})
+            }
+            </tr>
+    )
+}
 
 export default function Matching() {
     // Third iteration: Make work with state & events
-
-    const [cards, flipCard] = useState([
-        {id: 1, flipped: false, value: "😭"},
-        {id: 2, flipped: false, value: "🦁"},
-        {id: 3, flipped: false, value: "🦆"},
-        {id: 4, flipped: false, value: "😭"},
-        {id: 5, flipped: false, value: "🦆"},
-        {id: 6, flipped: false, value: "🦁"}
-    ])
-
-    const updateCard = (id) => {
-        // Add another useState to find out if matching here (if not - reflip all cards to back)
-        flipCard(cards.map(item => {
-            if (item.id === id) {
-                // The id matches so change flipped to true (to flip it)
-                return {...item, flipped: true};
-            }
-            return item
-        }))
-    }
 
     return (
         <>
             <p>Under Construction: Starting with board (all "flipped")</p>
             <table>
                 <tbody>
-                <tr>
-                    <td>
-                        <button onClick={() => updateCard(1)} className="btn-game">
-                            { cards[0].flipped ? cards[0].value : BACK }
-                        </button>
-                    </td>
-                    <td>
-                        <button onClick={() => updateCard(2)} className="btn-game">
-                            { cards[1].flipped ? cards[1].value : BACK }
-                        </button>
-                    </td>
-                    <td>
-                        <button onClick={() => updateCard(3)} className="btn-game">
-                            { cards[2].flipped ? cards[2].value : BACK }
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <button onClick={() => updateCard(4)} className="btn-game">
-                            { cards[3].flipped ? cards[3].value : BACK }
-                        </button>
-                    </td>
-                    <td>
-                        <button onClick={() => updateCard(5)} className="btn-game">
-                            { cards[4].flipped ? cards[4].value : BACK }
-                        </button>
-                    </td>
-                    <td>
-                        <button onClick={() => updateCard(6)} className="btn-game">
-                            { cards[5].flipped ? cards[5].value : BACK }
-                        </button>
-                    </td>
-                </tr>
+                    <Board />
                 </tbody>
             </table>
         </>
