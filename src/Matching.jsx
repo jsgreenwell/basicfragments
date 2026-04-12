7f2eb8bfc53d1d878e505ea8d8e798396348b5ac
JGreenwell <jsgreenwell83@gmail.com>
2026-04-12 10:38:01 -0500

    Its a working - if limited - game

--- src/Matching.jsx
+++ src/Matching.jsx
@@ -3,40 +3,39 @@ import {useState} from "react";
 
 export default function Matching() {
     // Third iteration: Make work with state & events
-    const BACK = "🎴"
-    const [clicker, setClicker] = useState({ctr: 0, idx: 0})
 
-    const [cards, setCards] = useState([
-        {id: 0, flipped: false, value: "😭", match: false},
-        {id: 1, flipped: false, value: "🦁", match: false},
-        {id: 2, flipped: false, value: "🦆", match: false},
-        {id: 3, flipped: false, value: "😭", match: false},
-        {id: 4, flipped: false, value: "🦆", match: false},
-        {id: 5, flipped: false, value: "🦁", match: false}
+    const BACK = "🎴"
+    const [clicker, setClicker] = useState({ctr: 0, idx: 0});
+
+    const [cards, flipCard] = useState([
+        {id: 0, flipped: false, value: "😭"},
+        {id: 1, flipped: false, value: "🦁"},
+        {id: 2, flipped: false, value: "🦆"},
+        {id: 3, flipped: false, value: "😭"},
+        {id: 4, flipped: false, value: "🦆"},
+        {id: 5, flipped: false, value: "🦁"}
     ])
 
     function Board() {
-        // This will just render the board
-        // So this was rendering twice - hate that needs a "prevent Default" - let's use map now
-        // Removed the board list - just map is fine - still need start & end indices so mutate local variable
-        // to hold all start/end ids (start will always have remainder of 1 (1 past 3) and 0 for end
+        // Build board the non-function
+        let board = []
         let indices = []
-        // console.log("Once or Twice?") // uncomment to check how many times this is rendering
 
         for (let i = 1; i <= cards.length; i++) {
-            if (i%3 === 0) {
-                // Slice ends with length (index to length) so +1 - hate JS slice
+            if (i % 3 === 0) {
+                // slice ends exclusive +1 length not index
                 indices.push(i)
-            } else if (i%3 === 1) {
-                // Slice starts with index so minus 1
+            } else if (i % 3 === 1) {
+                // slice starts (inclusive) -1 to meet index
                 indices.push(i-1)
             }
         }
 
-        // Now its dynamic (_ means ignore this value)
-        return indices.map((_, i) => {
-            return <BoardRow start={indices[i]} end={indices[i+1]} />
-        })
+        for (let i = 0; i < indices.length; i+=2) {
+            board.push(<BoardRow start={indices[i]} end={indices[i+1]} />)
+        }
+
+        return board
     }
 
     function BoardRow({start, end}) {
@@ -46,72 +45,76 @@ export default function Matching() {
                     cards.slice(start, end).map(card => {
                         return (
                             <td>
-                                <button className="btn-game" onClick={() => handleCard(card.id)}>
+                                <button onClick={() => handleCard(card.id)} className="btn-game">
                                     { card.flipped ? card.value : BACK }
                                 </button>
                             </td>
-                        )})
+                        )
+                    })
                 }
             </tr>
         )
     }
 
-    function handleCard(id) {
-        if (clicker.ctr === 0) {
-            // This is the first click so we just need to update the Card
-            setClicker({ctr: 1, idx: id})
-            updateCard(id)
+    function checkMatch(id) {
+        console.log(cards[id].value + " is now, it was " + cards[clicker.idx].value)
+
+        // Check new id vs. old id - or use it to check their values
+        // If match - update the card and set to match
+        if (cards[clicker.idx].value === cards[id].value) {
+            return "match"
         } else {
-            checkForMatch(id)
-            setClicker({ctr: 0, idx: 0})
+            return "nomatch"
         }
+
     }
 
-    function updateCard(id) {
-        console.log(id) // Just to check
-        setCards(cards.map(card => {
-            if (card.id === id && !card.match) {
-                // The id matches so change flipped to true (to flip it)
-                // Also check if its already been matched
-                return {...card, flipped: true};
-            }
-            return card
-        }))
+    function handleCard(id) {
+        // Check if this is first click or not
+        if (clicker.ctr === 0) {
+            // First click so just update the card to flipped and move on
+            setClicker({ctr: 1, idx: id});
+            updateCard(id, "first")
+        } else {
+            updateCard(id, checkMatch(id))
+            setClicker({ctr: 0, idx: 0});
+        }
+
     }
 
-    function checkForMatch(idx) {
-        console.log("new index: " + idx + " vs. old index: " + clicker.idx);
-        // If we have a match - then flip both cards & set to matched
-        // So this works
-        console.log(cards[clicker.idx].value);
-        console.log(cards[idx].value)
-
-        // Match - set values & leave alone
-            if (cards[clicker.idx].value === cards[idx].value) {
-                let indices = [clicker.idx, idx]
-                setCards(cards.map(card => {
-                    if (indices.includes(card.id)) {
-                        return {...card, flipped: true, match: true};
-                    } else {
-                        return card;
-                    }
-                }))
-            } else {
-                // Reset all the cards
-                setCards(cards.map(card => {
-                    if (!card.match) {
-                        return {...card, flipped: false};
-                    } else {
-                        return card;
-                    }
-                }))
-
-            }
+    const updateCard = (id, type) => {
+        // Add another useState to find out if matching here (if not - reflip all cards to back)
+        // if it's the first flip - just flips the card; match - flips both cards & sets match; third - flips all cards back (without match)
+        if (type === "match"){
+            flipCard(cards.map(item => {
+                if (item.id === id || item.id === clicker.idx) {
+                    // The id matches so change flipped to true (to flip it)
+                    return {...item, match: true, flipped: true};
+                }
+                return item
+            }))
+        } else if (type === "nomatch") {
+            flipCard(cards.map(item => {
+                if (!item.match) {
+                    // The id matches so change flipped to true (to flip it)
+                    return {...item, flipped: false};
+                }
+                return item
+            }))
+        } else {
+            flipCard(cards.map(item => {
+                if (item.id === id && !item.match) {
+                    // The id matches so change flipped to true (to flip it)
+                    return {...item, flipped: true};
+                }
+                return item
+            }))
+        }
     }
 
     return (
         <>
-            <p>Under Construction: Starting with board (all "flipped")</p>
+            <p>Under Construction: Sprint 1 complete: Stage 2 started</p>
             <table>
                 <tbody>
                     <Board />
